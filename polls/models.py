2f3f6ca9b3894cb621ff1d08cb133db56482c1e6
gintokius <develop17@datum-group.ru>
2018-09-24 15:04:08 +0300

    Add test and new template

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,6 +23,5 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
