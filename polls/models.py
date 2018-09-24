7e1ece9b663a8eae6a267cd1a93c919a73b358f2
gintokius <develop17@datum-group.ru>
2018-09-24 15:31:31 +0300

    Finish django site tutorial

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
