2b028e12da415b8ba7265452b8ea747879f6ef8f
gintokius <develop17@datum-group.ru>
2018-09-25 10:06:45 +0300

    Finish testing

--- pollsapi/tests.py
+++ pollsapi/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase, APIRequestFactory, APIClient
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from pollsapi import views
+
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.factory = APIRequestFactory()
+        self.view = views.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        user = User.objects.create_user('test',
+                                        email='test@user.com',
+                                        password='test'
+                                        )
+        Token.objects.create(user=user)
+        return user
+
+    def test_list(self):
+        request = self.factory.get(self.uri)
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response code 200, received {0} instead.'.format(response.status_code))
+
+    def test_list2(self):
+        self.client.login(username="test", password="test")
+        response = self.client.get(self.uri)
+        self.assertEqual(response.status_code, 200,
+                         'Expected Response code 200, received {0} instead.'.format(response.status_code))
+
--- pollsapi/views.py
+++ pollsapi/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate
 from rest_framework import generics, viewsets
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,12 +21,24 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
+
 
 class ChoiceList(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
         return queryset
     serializer_class = ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs["pk"])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
 
 
 class CreateVote(APIView):
